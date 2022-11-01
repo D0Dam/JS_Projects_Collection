6bd79326417fc9b0862f93cbedbf212b4c92f18c
minjaeKim <smallkdb@gmail.com>
2022-11-01 10:30:09 +0900

    refactor: moveball 문제 리팩터링

--- modernJS/Part2/event/browserEventIntro/task4(MoveBall)/index.js
+++ modernJS/Part2/event/browserEventIntro/task4(MoveBall)/index.js
@@ -1,14 +1,21 @@
 function moveball(e) {
   const ball  = document.querySelector("#ball");
+  const field = document.querySelector("#box");
+  const fieldPosition = field.getBoundingClientRect();
   ball.style.position = "absolute";
-  let ballLeft  = e.clientX - 24;
-  let ballTop  = e.clientY - 24;
-  if(e.clientX < 24) ballLeft = 0;
-  if(e.clientX > 216) ballLeft = 196;
-  if(e.clientY < 24) ballTop = 0;
-  if(e.clientY > 136) ballTop = 116;
-  ball.style.left = ballLeft.toString() + "px";
-  ball.style.top = ballTop.toString() + "px";
+
+  let ballPosition = {
+    left : e.clientX - fieldPosition.left - field.clientLeft - ball.clientWidth / 2,
+    top : e.clientY - fieldPosition.top - field.clientTop - ball.clientHeight / 2,
+  }
+
+  if(ballPosition.left < ball.clientLeft) ballPosition.left = 0;
+  if(ballPosition.top < ball.clientTop) ballPosition.top = 0;
+  if(ballPosition.left > field.clientWidth - ball.clientWidth) ballPosition.left = field.clientWidth - ball.clientWidth;
+  if(ballPosition.top > field.clientHeight - ball.clientHeight) ballPosition.top = field.clientHeight - ball.clientHeight;
+
+  ball.style.left = ballPosition.left.toString() + "px";
+  ball.style.top = ballPosition.top.toString() + "px";
 }
 
 document.querySelector("#box").addEventListener('click', moveball);
