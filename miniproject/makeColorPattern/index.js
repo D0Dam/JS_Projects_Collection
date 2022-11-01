785e031d5673b872ca7013cca00c32cd47590aa3
minjaeKim <smallkdb@gmail.com>
2022-11-01 17:46:23 +0900

    refactor: 무지개 출력기 메인 함수 클로저 활용

--- miniproject/makeColorPattern/index.js
+++ miniproject/makeColorPattern/index.js
@@ -1,14 +1,33 @@
 const colorSpaces = document.querySelectorAll('.colorspace');
-const COLOR = ['#ef3550', '#f9a825', '#eeff41', '#43a047', '#0000ff', '#000080', '#7e57c2'];
+const RAINBOW_COLOR = ['#ef3550', '#f9a825', '#eeff41', '#43a047', '#0000ff', '#000080', '#7e57c2'];
+
+// counter를 함수 안에 넣고 클로저로 운영하는 경우
+function getColorPattern() {
+  let counter = 0;
+  return () => {
+    const fillNumber = counter % colorSpaces.length;
+    const color = counter % RAINBOW_COLOR.length;
+    colorSpaces.forEach((space, index) => {
+      fillNumber===index ?
+      space.style.background = RAINBOW_COLOR[color] : space.style.background = "white"
+    });
+    counter += 1;
+    return counter;
+  }
+}
+
+// counter를 전역변수로 선언한 경우
+/*
 let counter = 0;
 function getColorPattern() {
   const fillNumber = counter % colorSpaces.length;
-  const pickColor = counter % COLOR.length;
+  const color = counter % RAINBOW_COLOR.length;
   colorSpaces.forEach((space, index) => {
     fillNumber===index ?
-    space.style.background = COLOR[pickColor] : space.style.background = "white"
+    space.style.background = RAINBOW_COLOR[color] : space.style.background = "white"
   });
   counter += 1;
 }
+*/
 
-document.querySelector('#colorbutton').addEventListener('click', getColorPattern);
+document.querySelector('.colorbutton').addEventListener('click', getColorPattern());
